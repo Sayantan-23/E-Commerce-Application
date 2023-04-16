f14d51b8dfcb67aec1ba006d80bfbfe132d24370
Sayantan <sayantan358@gmail.com>
2023-04-17 02:02:17 +0530

    added jwt and forgot password functionality

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,10 +1,12 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/ecom"
-}
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/ecom",
+  JWT_SECRET: process.env.JWT_SECRET || "myrandomsecret",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "7d",
+};
 
-export default config
+export default config;
